7d9528d403eda0a05dc7949810ac0dec2acab3d2
calexander618 <43504910+calexander618@users.noreply.github.com>
2019-04-13 22:31:59 -0500

    Add files via upload

---
+++ image_assessment.sql
@@ -0,0 +1,24 @@
+ALTER TABLE `Images` DROP FOREIGN KEY `fk_Photo_User_1`;
+
+DROP TABLE `Images`;
+DROP TABLE `Users`;
+
+CREATE TABLE `Images` (
+`ImageID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`FilePath` varchar(60) NOT NULL,
+`UploadDate` date NOT NULL,
+`UploaderID` varchar(12) NOT NULL,
+`AestheticScore` float UNSIGNED NULL,
+`TechnicalScore` float UNSIGNED NULL,
+PRIMARY KEY (`ImageID`) 
+);
+CREATE TABLE `Users` (
+`UserID` varchar(12) NOT NULL,
+`Password` varchar(12) NOT NULL,
+`FullName` varchar(30) NULL,
+`PermissionLevel` int(1) UNSIGNED NOT NULL DEFAULT 0,
+PRIMARY KEY (`UserID`) 
+);
+
+ALTER TABLE `Images` ADD CONSTRAINT `fk_Photo_User_1` FOREIGN KEY (`UploaderID`) REFERENCES `Users` (`UserID`);
+
