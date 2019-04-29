ff531c34086ff7bfa90c9f2c5e913f6b47a6b85e
Gavin Lee <gavinleeke@outlook.com>
2019-04-29 10:23:15 -0500

    Update SendNotificationEmail.sh

--- image-assessment/app/Scripts/SendNotificationEmail.sh
+++ image-assessment/app/Scripts/SendNotificationEmail.sh
@@ -1,8 +1,8 @@
 /root/swaks --auth \
 	--server smtp.mailgun.org:587 \
-	--au postmaster@sandbox32a74136f3ea4c50a9726b45d6495be6.mailgun.org \
-	--ap e32e047e9b5140dff1ced04ab730fb93-dc5f81da-39bed3e1 \
-	--from postmaster@sandbox32a74136f3ea4c50a9726b45d6495be6.mailgun.org \
+	--au ############################################# \
+	--ap ######################################################## \
+	--from ########################################################### \
 	--to $1 \
 	--h-Subject: 'Photos Scored' \
 	--body 'The useID is '$2.' And the submissionID is '$3
