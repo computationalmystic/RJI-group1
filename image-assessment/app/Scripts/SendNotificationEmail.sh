e5246bdce53c2b7106d023284b667d0ee741151e
Gavin Lee <gavinleeke@outlook.com>
2019-04-30 22:52:50 -0500

    Update SendNotificationEmail.sh

--- image-assessment/app/Scripts/SendNotificationEmail.sh
+++ image-assessment/app/Scripts/SendNotificationEmail.sh
@@ -1,15 +1,10 @@
 /root/swaks --auth \
 	--server smtp.mailgun.org:587 \
-<<<<<<< HEAD
-	--au postmaster@mg.rjimizzou.info \
-	--ap 906bd5667341bfa3c02a12560ec851e0-7bce17e5-1ecaa3c6 \
-	--from noreply@rjimizzou.info \
-=======
 	--au ##################################################### \
 	--ap ######################################################### \
 	--from ############################################################ \
->>>>>>> 8b728c12421106b220031e66d3f1f57215a56d4c
-	--to $1 \
+	
+    --to $1 \
 	--h-Subject: 'Photos Scored' \
 	--body 'Your photos have been analyzed! Visit this link to download the scored submission: https://rjimizzou.info/download/'$2'/'$3
 	
