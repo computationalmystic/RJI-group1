bba59f8aefff0914862c31c7a3266c3325a0e367
Gavin Lee <gavinleeke@outlook.com>
2019-05-18 19:20:35 -0500

    remove

--- html&CSS/getScoreAestheticAPI.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash  
-
-cd /home/ubuntu/model/RJI-group1-model/ && source contrib/tf_serving/venv_tfs_nima/bin/activate && python -W ignore -m contrib.tf_serving.tfs_sample_client --image-path /home/ubuntu/photos/users/$NAME --model-name mobilenet_aesthetic
-
